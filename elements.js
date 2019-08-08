d41f04b48624c6a80251bcfa5eb4568ac9ee8832
Nataliia <dykunnata@gmail.com>
2019-08-08 13:03:15 -0700

    contact me page

---
+++ elements.js
@@ -0,0 +1,31 @@
+// function fade() {
+//     $('.preloader').fadeOut("slow");
+//     }
+//     setTimeout(fade, 3000);
+//     console.log("hello");
+    
+//     setTimeout( function() {
+//     $(".hiddenSlide").fadeIn();
+//     }, 3500);
+     
+//     setTimeout( function() {
+//     $("#fadeIn").fadeIn();
+//     }, 3700);
+    
+//     setTimeout( function() {
+//     $("#tourFade").fadeIn();
+//     }, 3900);
+    
+//     $(".toggle").click(function(){
+//       $(this).find(".toggleContainer").toggleClass("active");
+//       $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
+//     });
+    
+jQuery(document).ready(function($) {
+    var Body = $('body');
+    Body.addClass('preloader-site');
+});
+$(window).load(function(){
+    $('.preloader-wrapper').fadeOut();
+    $('body').removeClass('preloader-site');
+});
